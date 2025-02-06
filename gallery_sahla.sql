e5c4e3d081c54e9afa300a208160b605751c27f6
sahlaAja <sahlafr10@gmail.com>
2025-02-06 15:08:24 +0700

    ubah database

--- gallery_sahla.sql
+++ gallery_sahla.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 05 Feb 2025 pada 08.51
+-- Waktu pembuatan: 06 Feb 2025 pada 09.07
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -66,11 +66,11 @@ CREATE TABLE `foto` (
 --
 
 INSERT INTO `foto` (`foto_id`, `judul_foto`, `deskripsi`, `tanggal_unggah`, `lokasi_file`, `album_id`, `user_id`) VALUES
-(2, 'SMA ', 'lagi nanam kapsul waktu hehe', '2025-02-05', 'lovely4.jpg', 2, 3),
-(3, 'photobooth', 'foto date', '2025-02-05', 'lovely2.jpg', 3, 4),
-(4, 'Im Sol', 'beautiful & cute', '2025-02-05', 'imsol2.jpg', 2, 3),
-(5, 'nasi goreng ', 'nasi goreng ala korea', '2025-02-05', 'food.jpg', 4, 2),
-(6, 'ootd #1', 'pinky outfit', '2025-02-05', 'user2.jpg', 5, 2);
+(1, 'photobooth', 'first time foto with sunjae', '2025-02-06', 'lovely2.jpg', 3, 4),
+(2, 'SMA ERA', 'lagi nanam kapsul waktu ', '2025-02-06', 'lovely4.jpg', 2, 3),
+(3, 'ootd #1', 'outfit', '2025-02-06', 'ootd3.jpg', 5, 2),
+(4, 'ootd #2', 'outfit', '2025-02-06', 'ootd2.jpg', 5, 2),
+(5, 'summer sea cafe', 'hunting cafe', '2025-02-06', 'cafe.jpg', 4, 2);
 
 -- --------------------------------------------------------
 
@@ -85,23 +85,6 @@ CREATE TABLE `komentar_foto` (
   `isi_komentar` text NOT NULL,
   `tanggal_komentar` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data untuk tabel `komentar_foto`
---
-
-INSERT INTO `komentar_foto` (`komentar_id`, `foto_id`, `user_id`, `isi_komentar`, `tanggal_komentar`) VALUES
-(1, 2, 2, 'lucu banget kakkk\r\n', '2025-02-05'),
-(2, 4, 2, '😍😍😍', '2025-02-05'),
-(3, 5, 2, 'buat yang mau resep nya comment yaa nanti aku drop disini!!!', '2025-02-05'),
-(4, 4, 4, '🦹‍♀️🦹‍♀️🦹‍♀️', '2025-02-05'),
-(5, 3, 4, 'lucuuu\r\n', '2025-02-05'),
-(6, 5, 4, 'akuu mau dong kakkk', '2025-02-05'),
-(7, 6, 4, 'cantik sekali kak!!', '2025-02-05'),
-(8, 3, 2, 'lucu kalian bersinar banget ☀️☀️☀️☀️☀️', '2025-02-05'),
-(9, 5, 2, '(1) 4 piring nasi putih dingin \r\n(2) 1 mangkuk kecil kimchi, potong tipis 75 gram \r\n(3) jamur enoki 150 gram \r\n(4) daging asap, potong tipis \r\n(5) 2 siung bawang putih, memarkan, iris halus \r\n(6) 1 sdm minyak wijen \r\n(7) 1 sdm minyak zaitun \r\n(8) 4 butir telur, masak telur mata sapi \r\n(9) 2 sdm biji wijen, sangrai \r\n(10) Garam dan gula secukupnya\r\n', '2025-02-05'),
-(10, 5, 2, 'untuk cara masaknya sama ajaa kayak masak nasi goreng biasa ya im sol', '2025-02-05'),
-(11, 6, 2, 'makasih im sol 🧡🧡🧡', '2025-02-05');
 
 -- --------------------------------------------------------
 
@@ -121,16 +104,8 @@ CREATE TABLE `like_foto` (
 --
 
 INSERT INTO `like_foto` (`like_id`, `foto_id`, `user_id`, `tanggal_like`) VALUES
-(2, 2, 4, '2025-02-05'),
-(3, 3, 3, '2025-02-05'),
-(5, 4, 2, '2025-02-05'),
-(6, 3, 2, '2025-02-05'),
-(7, 2, 2, '2025-02-05'),
-(8, 4, 4, '2025-02-05'),
-(9, 6, 4, '2025-02-05'),
-(10, 5, 4, '2025-02-05'),
-(11, 6, 3, '2025-02-05'),
-(12, 5, 3, '2025-02-05');
+(1, 1, 4, '2025-02-06'),
+(2, 1, 3, '2025-02-06');
 
 -- --------------------------------------------------------
 
@@ -152,29 +127,13 @@ CREATE TABLE `notifikasi` (
 --
 
 INSERT INTO `notifikasi` (`id`, `penerima_id`, `pengirim_id`, `message`, `is_read`, `created_at`) VALUES
-(1, 3, 3, '@sunjae menyukai postingan SMA ERA Anda ', 1, '2025-02-05 05:01:46'),
-(2, 3, 4, '@imsol menyukai postingan SMA  Anda ', 1, '2025-02-05 06:41:08'),
-(3, 4, 3, '@sunjae menyukai postingan photobooth Anda ', 1, '2025-02-05 06:44:54'),
-(4, 3, 2, '@sahlaaja menyukai postingan SMA  Anda ', 1, '2025-02-05 06:48:12'),
-(5, 3, 2, '@sahlaaja menyukai postingan Im Sol Anda ', 1, '2025-02-05 06:48:14'),
-(6, 4, 2, '@sahlaaja menyukai postingan photobooth Anda ', 1, '2025-02-05 06:48:16'),
-(7, 3, 2, '@sahlaaja memberi komentar -lucu banget kakkk\r\n- pada postingan SMA  Anda ', 1, '2025-02-05 06:48:42'),
-(8, 3, 2, '@sahlaaja menyukai postingan SMA  Anda ', 1, '2025-02-05 06:49:18'),
-(9, 3, 2, '@sahlaaja memberi komentar -😍😍😍- pada postingan Im Sol Anda ', 1, '2025-02-05 06:50:16'),
-(10, 2, 2, '@sahlaaja memberi komentar -buat yang mau resep nya comment yaa nanti aku drop disini!!!- pada postingan nasi goreng  Anda ', 1, '2025-02-05 06:52:31'),
-(11, 3, 4, '@imsol menyukai postingan Im Sol Anda ', 1, '2025-02-05 06:56:48'),
-(12, 3, 4, '@imsol memberi komentar -🦹‍♀️🦹‍♀️🦹‍♀️- pada postingan Im Sol Anda ', 1, '2025-02-05 06:58:12'),
-(13, 4, 4, '@imsol memberi komentar -lucuuu\r\n- pada postingan photobooth Anda ', 1, '2025-02-05 06:58:45'),
-(14, 2, 4, '@imsol menyukai postingan ootd #1 Anda ', 1, '2025-02-05 06:59:02'),
-(15, 2, 4, '@imsol menyukai postingan nasi goreng  Anda ', 1, '2025-02-05 06:59:06'),
-(16, 2, 4, '@imsol memberi komentar -akuu mau dong kakkk- pada postingan nasi goreng  Anda ', 1, '2025-02-05 06:59:55'),
-(17, 2, 4, '@imsol memberi komentar -cantik sekali kak!!- pada postingan ootd #1 Anda ', 1, '2025-02-05 07:00:13'),
-(18, 2, 3, '@sunjae menyukai postingan ootd #1 Anda ', 1, '2025-02-05 07:01:41'),
-(19, 2, 3, '@sunjae menyukai postingan nasi goreng  Anda ', 1, '2025-02-05 07:01:43'),
-(20, 4, 2, '@sahlaaja memberi komentar -lucu kalian bersinar banget ☀️☀️☀️☀️☀️- pada postingan photobooth Anda ', 0, '2025-02-05 07:04:50'),
-(21, 2, 2, '@sahlaaja memberi komentar -(1) 4 piring nasi putih dingin \r\n(2) 1 mangkuk kecil kimchi, potong tipis 75 gram \r\n(3) jamur enoki 150 gram \r\n(4) daging asap, potong tipis \r\n(5) 2 siung bawang putih, memarkan, iris halus \r\n(6) 1 sdm minyak wijen \r\n(7) 1 sdm ', 1, '2025-02-05 07:08:32'),
-(22, 2, 2, '@sahlaaja memberi komentar -untuk cara masaknya sama ajaa kayak masak nasi goreng biasa ya im sol- pada postingan nasi goreng  Anda ', 1, '2025-02-05 07:09:13'),
-(23, 2, 2, '@sahlaaja memberi komentar -makasih im sol 🧡🧡🧡- pada postingan ootd #1 Anda ', 1, '2025-02-05 07:16:13');
+(26, 4, 4, '@imsol menyukai postingan photobooth Anda ', 1, '2025-02-06 01:32:17'),
+(27, 4, 4, '@imsol menyukai postingan photobooth Anda ', 1, '2025-02-06 01:35:56'),
+(28, 4, 3, '@sunjae menyukai postingan photobooth Anda ', 1, '2025-02-06 01:36:23'),
+(29, 2, 4, '@imsol menyukai postingan nasi goreng  Anda ', 1, '2025-02-06 04:04:13'),
+(30, 2, 4, '@imsol menyukai postingan ootd #1 Anda ', 1, '2025-02-06 04:05:18'),
+(31, 4, 4, '@imsol menyukai postingan photobooth Anda ', 1, '2025-02-06 07:34:30'),
+(32, 4, 3, '@sunjae menyukai postingan photobooth Anda ', 1, '2025-02-06 08:03:42');
 
 -- --------------------------------------------------------
 
@@ -219,7 +178,7 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`user_id`, `role_id`, `username`, `password`, `email`, `name`, `alamat`, `foto_profil`, `verifikasi`) VALUES
 (1, 1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin@gmail.com', 'Jaya', 'Cipageran Asri', 'admin.jpg', 1),
-(2, 2, 'sahlaaja', '1dcb74b1127cad99e3ab47eee5583187', 'sahlafr10@gmail.com', 'Sahla Fadhilah', 'jl. Puri Cipageran Indah 1 H4 47', 'user2.jpg', 1),
+(2, 2, 'sahlaaja', '1dcb74b1127cad99e3ab47eee5583187', 'sahlafr10@gmail.com', 'Sahla Fadhilah Rahim', 'jl. Puri Cipageran Indah 1 H4 47', 'user2.jpg', 1),
 (3, 2, 'sunjae', '36e869447c142f993a15934e0297921d', 'sunjae@gmail.com', 'Ryu Sunjae', 'Seoul, South Korea', 'sunjae.jpg', 1),
 (4, 2, 'imsol', '746d6941f39bf1df6622b99ac44e5163', 'ImSol@gmail.com', 'Im Sol', 'Gangnam, South Korea', 'sol.jpg', 1),
 (5, 2, 'ajeliaaa', 'd74362f2d3b4ecd37daacf79d1bc2fa5', 'ajelia@gmail.com', 'Nadhira Azalea', 'GBR 3', 'user3.jpg', 1);
@@ -292,25 +251,25 @@ ALTER TABLE `album`
 -- AUTO_INCREMENT untuk tabel `foto`
 --
 ALTER TABLE `foto`
-  MODIFY `foto_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `foto_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `komentar_foto`
 --
 ALTER TABLE `komentar_foto`
-  MODIFY `komentar_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `komentar_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `like_foto`
 --
 ALTER TABLE `like_foto`
-  MODIFY `like_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `like_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `notifikasi`
 --
 ALTER TABLE `notifikasi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT untuk tabel `role`
